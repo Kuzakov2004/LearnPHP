c2a0ed5d63c983737d1f7deaffa4b30005f6d5b1
Kuzakov2004 <154569259+Kuzakov2004@users.noreply.github.com>
2026-01-14 03:23:29 +0700

    Delete src directory

--- src/routes.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-    '~^hello/(.*)$~' => [\MyProject\Controllers\MainController::class, 'sayHello'],
-    '~^bye/(.*)$~' => [\MyProject\Controllers\MainController::class, 'sayBye'],
-    '~^$~' => [\MyProject\Controllers\MainController::class, 'main'],
-];
